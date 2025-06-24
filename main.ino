a8075cad8eb38cedacf5282068ea9bb6fa387663
ejfaoiwejf <aewfae@gmail.com>
2025-06-24 23:55:28 +0300

    fix

--- main.ino
+++ main.ino
@@ -3,37 +3,49 @@
 #include <LiquidCrystal.h>
 #include <TimerOne.h>
 
-// Изменим пины дисплея на более свободные:
+//Пины для подключения дисплея.
 const int rs = A0, en = A1, d4 = A2, d5 = A3, d6 = A4, d7 = A5;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 #define DEBUG
-#define ONE_WIRE_BUS 2 // Pin for the temperature sensors
+//Пин для подключения датчиков температуры.
+#define ONE_WIRE_BUS 4 
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
+//Пин вывода сигнала тревога.
 #define ALARM_PIN 13
 
-#define FAN1_PWM_PIN 3
-#define FAN2_PWM_PIN 5
-#define FAN3_PWM_PIN 6
+//Пины для вывода ШИМ.
+#define FAN1_PWM_PIN 9
+#define FAN2_PWM_PIN 10
+#define FAN3_PWM_PIN 3
 
+//Кол-во записей в таблице с привязкой температуры к скважности.
 #define FAN1_ROWS_COUNT 11
 #define FAN2_ROWS_COUNT 11
 #define FAN3_ROWS_COUNT 11
 
+//Если хоть 1 датчик имеет температуру более, то включаем тревогу.
 #define FAN1_ALARM_TEMP 95
 #define FAN2_ALARM_TEMP 95
 #define FAN3_ALARM_TEMP 95
 
+//Нижний предел, если включена тревога и температура ниже этого предела, то выключаем тревогу.
+#define FAN1_ALARM_RESET_TEMP 50
+#define FAN2_ALARM_RESET_TEMP 50
+#define FAN3_ALARM_RESET_TEMP 50
+
+
+
 float fan_1_comparison[FAN1_ROWS_COUNT][2] = {
   {0, 0},
-  {10, 100},
-  {20, 200},
-  {30, 300},
-  {40, 400},
-  {50, 500},
+  {10, 0},
+  {20, 0},
+  {30, 0},
+  {40, 40},
+  {50, 50},
   {60, 600},
   {70, 700},
   {80, 800},
@@ -43,11 +55,11 @@ float fan_1_comparison[FAN1_ROWS_COUNT][2] = {
 
 float fan_2_comparison[FAN2_ROWS_COUNT][2] = {
   {0, 0},
-  {10, 100},
-  {20, 200},
-  {30, 300},
-  {40, 400},
-  {50, 500},
+  {10, 0},
+  {20, 0},
+  {30, 0},
+  {40, 40},
+  {50, 50},
   {60, 600},
   {70, 700},
   {80, 800},
@@ -57,11 +69,11 @@ float fan_2_comparison[FAN2_ROWS_COUNT][2] = {
 
 float fan_3_comparison[FAN3_ROWS_COUNT][2] = {
   {0, 0},
-  {10, 100},
-  {20, 200},
-  {30, 300},
-  {40, 400},
-  {50, 500},
+  {10, 0},
+  {20, 0},
+  {30, 0},
+  {40, 40},
+  {50, 50},
   {60, 600},
   {70, 700},
   {80, 800},
@@ -76,14 +88,42 @@ void setup()
   #endif //DEBUG
   sensors.begin();
 
+  pinMode(FAN1_PWM_PIN, OUTPUT);
+  pinMode(FAN2_PWM_PIN, OUTPUT);
+  pinMode(FAN3_PWM_PIN, OUTPUT);
+
   pinMode(ALARM_PIN, OUTPUT);
 
-  // Initialize TimerOne for 25 kHz PWM
+  // Инициализируем TimerOne на частоте 25 kHz ШИМ
   Timer1.initialize(40); // 40 microseconds for 25 kHz
-  Timer1.attachInterrupt(updatePWM); // Attach the interrupt function
 
   lcd.begin(16, 2);
+
+  /*
+  TCCR2A = 0;               // Сбрасываем регистр управления таймером 2
+TCCR2B = 0;               // Сбрасываем регистр управления таймером 2
+TCNT2 = 0;                // Устанавливаем начальное значение счетчика таймера 2
+TCCR2A |= (1 << COM2B1);  // Устанавливаем режим non-inverting ШИМ на пине OC2B (пин 10)
+TCCR2A |= (1 << WGM21) | (1 << WGM20); // Устанавливаем режим Fast PWM
+TCCR2B |= (1 << CS20);    // Устанавливаем предделитель на 1
+
+OCR2A = 639;  
+*/
+
+
+TCCR2A = 0;
+TCCR2B = 0;
+TCNT2 = 0;
+
+TCCR2A |= (1 << COM2B1) | (1 << WGM21) | (1 << WGM20); // non-inverting PWM, Fast PWM режим 7 (WGM22=1)
+TCCR2B |= (1 << WGM22) | (1 << CS11);  // WGM22=1 (режим 7), предделитель = 8 (CS11)
+
+OCR2A = 79; // TOP = 79 -> частота ~25 кГц
+
 }
+
+bool f_alarm = false;
+
 
 void alarm(bool state)
 {
@@ -110,23 +150,25 @@ float findClosestValue(float fan_comparison[][2], int rows_count, float targetTe
   return closestValue;
 }
 
-void updatePWM()
-{
-  static float fan1Speed = 0;
-  static float fan2Speed = 0;
-  static float fan3Speed = 0;
 
-  // Update PWM values based on the latest temperatures
-  analogWrite(FAN1_PWM_PIN, fan1Speed);
-  analogWrite(FAN2_PWM_PIN, fan2Speed);
-  analogWrite(FAN3_PWM_PIN, fan3Speed);
+
+
+
+void setPWMDuty_D3(uint16_t duty)
+{
+  if (duty > 1024) duty = 1024;
+  Serial.print("duty: ");
+  Serial.println((uint32_t)duty * OCR2A / 1024);
+  OCR2B = (uint8_t)((uint32_t)duty * OCR2A / 1024);
 }
+
+
+
 
 void loop()
 {
   sensors.requestTemperatures();
 
-  bool f_alarm = false;
   float temp[3];
 
   for (int i = 0; i < 3; ++i) {
@@ -143,37 +185,49 @@ void loop()
   if (temp[2] > FAN3_ALARM_TEMP) f_alarm = true;
   float fan3Speed = findClosestValue(fan_3_comparison, FAN3_ROWS_COUNT, temp[2]);
 
-  // Update the PWM values for the fans
-  Timer1.setPwmDuty(FAN1_PWM_PIN, fan1Speed);
-  Timer1.setPwmDuty(FAN2_PWM_PIN, fan2Speed);
-  Timer1.setPwmDuty(FAN3_PWM_PIN, fan3Speed);
+  if(f_alarm &&
+     temp[0] < FAN1_ALARM_RESET_TEMP &&
+     temp[1] < FAN2_ALARM_RESET_TEMP &&
+     temp[2] < FAN3_ALARM_RESET_TEMP)
+  {
+    f_alarm = false;
+  }
+
+  
+  Timer1.pwm(FAN1_PWM_PIN, fan1Speed);
+  Timer1.pwm(FAN2_PWM_PIN, fan2Speed);
+  setPWMDuty_D3(fan3Speed);
+  //Timer1.pwm(FAN3_PWM_PIN, fan3Speed);
 
   // Display on LCD
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("0:");
-  lcd.print(temp[0], 1);
-  lcd.print(" 1:");
-  lcd.print(temp[1], 1);
+  static unsigned long lastUpdate = 0;
+  if (millis() - lastUpdate >= 500) {  // обновляем каждые 500 мс
+    lastUpdate = millis();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("0:");
+    lcd.print(temp[0], 1);
+    lcd.print(" 1:");
+    lcd.print(temp[1], 1);
 
-  lcd.setCursor(0, 1);
-  lcd.print("2:");
-  lcd.print(temp[2], 1);
-  lcd.print(f_alarm ? " ALARM" : " OK");
+    lcd.setCursor(0, 1);
+    lcd.print("2:");
+    lcd.print(temp[2], 1);
+    lcd.print(f_alarm ? " ALARM" : " OK");
 
-  alarm(f_alarm);
+    alarm(f_alarm);
 
-  #ifdef DEBUG
-  Serial.print("Тревога: ");
-  Serial.println(f_alarm);
-  for (int i = 0; i < 3; ++i) {
-    Serial.print("Температура ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(temp[i]);
+    #ifdef DEBUG
+    Serial.print("Тревога: ");
+    Serial.println(f_alarm);
+    for (int i = 0; i < 3; ++i) {
+      Serial.print("Температура ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(temp[i]);
+    }
+    #endif // DEBUG
   }
-  #endif // DEBUG
-
-  delay(1000); // Update every second
+  
 }
 
